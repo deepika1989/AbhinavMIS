59af9cf6cbbeb7df920d916ed7fd73b358f75d33
Deepika Jagtap <email.example.adcs@gmail.com>
2017-11-21 19:55:08 +0530

    Signed-off-by: Deepika Jagtap <email.example.adcs@gmail.com>

---
+++ plugins/cordova-plugin-globalization/src/blackberry10/native/public/json_value.cpp
@@ -0,0 +1,11 @@
+// Copyright 2007-2010 Baptiste Lepilleur
+// Distributed under MIT license, or public domain if desired and
+// recognized in your jurisdiction.
+// See file LICENSE for detail or copy at http://jsoncpp.sourceforge.net/LICENSE
+
+/*
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, s
